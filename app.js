9e5031eff66e8dc8ae3695c064c8e02799bc4b3e
Wadie Benabdouh <wadie.trade@gmail.com>
2023-10-03 02:03:01 +0100

    main

--- app.js
+++ app.js
@@ -26,11 +26,14 @@ closeSearchBtn.onclick = () => {
 const burgerMenuBtn = document.querySelector("#burgerMenuBtn")
 const mobileNav = document.querySelector(".mobile-nav")
 const closeBurgerBtn = document.querySelector("#closeBurger")
+const bodyOverlayDark = document.querySelector("body")
 
 burgerMenuBtn.onclick = () => {
     mobileNav.classList.toggle("active")
+    bodyOverlayDark.classList.toggle("active")
 }
 
 closeBurgerBtn.onclick = () => {
     mobileNav.classList.remove("active")
+    bodyOverlayDark.classList.remove("active")
 }
